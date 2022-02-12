cdba81a908fd79fe640cb25e7e3ce97ea187316d
legabi <legabi.dev@gmail.com>
2022-02-12 15:57:20 +0100

    contact.php

--- contact.php
+++ contact.php
@@ -1,13 +1,22 @@
 <?php include 'nav.php'; ?>
-<?php include 'database.php'; ?>
+<?php include 'database.php';
+// require "/home/gabi/myplugins/api/assets/mail.php";
+?>
 <?php
 if (!empty($_POST)) {
   $mail = $_POST['mail'];
   $message = $_POST['message'];
   $object = $_POST['object'];
   if (!empty($mail) && !empty($message) && !empty($object)) {
-    $req = $db->prepare("INSERT INTO contact SET mail = ?, message = ?, object = ?, date = NOW()");
-    $req->execute([$mail, $message, $object]);
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+      $errors['email'] = "Votre email n'est pas valide";
+    }
+    if (empty($errors)) {
+      $req = $db->prepare("INSERT INTO contact SET mail = ?, message = ?, object = ?, date = NOW()");
+      $req->execute([$mail, $message, $object]);
+      //send_mail($mail, "Confirmation", "Vous avez envoyer un message au site https://agamiweb.ml/");
+      header("Location: /");
+    }
   } else {
     $errors["field"] = "Veuillez remplir tous les champs";
   }
@@ -62,10 +71,9 @@ if (!empty($_POST)) {
 
       <div id="message">
         <p>Vous n'avez pas remplis le fomulaire correctement</p>
-        <p id="length_msg">Votre message ne peut pas être vide</p>
         <p id="length_object">Votre objet ne peut pas être vide</p>
         <p id="length_mail">Votre mail ne peut pas être vide</p>
-        <p id="length_mail">Votre mail n'est pas valide</p>
+        <p id="valid_mail">Votre mail n'est pas valide</p>
       </div>
 
       <form method="POST" action="">
@@ -81,7 +89,7 @@ if (!empty($_POST)) {
 
         <div class="form-group">
           <label for="message">Message</label>
-          <textarea class="form-control" rows="3" id="message" name="message" required /></textarea>
+          <textarea type="text" class="form-control" rows="3" id="message" name="message" required></textarea>
         </div>
 
         <button type="submit" class="btn btn-primary">Envoyer un message</button>
@@ -96,38 +104,30 @@ if (!empty($_POST)) {
         var object = document.getElementById("object");
         var length_object = document.getElementById("length_object");
 
-        //Msg
-        var msg = document.getElementById("message");
-        var length_msg = document.getElementById("length_msg");
+        length_mail.classList.remove("valid");
+        length_mail.classList.add("invalid");
+        length_object.classList.remove("valid");
+        length_object.classList.add("invalid");
 
-        length_pswd.classList.remove("valid");
-        length_pswd.classList.add("invalid");
-        length_pseudo.classList.remove("valid");
-        length_pseudo.classList.add("invalid");
-
-        pswd.onkeyup = function() {
-          // Validate length
-          if (pswd.value.length >= 8) {
-            length_pswd.classList.remove("invalid");
-            length_pswd.classList.add("valid");
+        mail.onkeyup = function() {
+          if (mail.value.length > 0) {
+            length_mail.classList.remove("invalid");
+            length_mail.classList.add("valid");
           } else {
-            length_pswd.classList.remove("valid");
-            length_pswd.classList.add("invalid");
+            length_mail.classList.remove("valid");
+            length_mail.classList.add("invalid");
           }
         }
 
-        pseudo.onkeyup = function() {
-          // Validate length
-          if (pseudo.value.length >= 3) {
-            length_pseudo.classList.remove("invalid");
-            length_pseudo.classList.add("valid");
+        object.onkeyup = function() {
+          if (object.value.length > 0) {
+            length_object.classList.remove("invalid");
+            length_object.classList.add("valid");
           } else {
-            length_pseudo.classList.remove("valid");
-            length_pseudo.classList.add("invalid");
+            length_object.classList.remove("valid");
+            length_object.classList.add("invalid");
           }
         }
-
-        /^[a-zA-Z0-9_]+$/.test(pseudo)
       </script>
     </div>
   </main>
