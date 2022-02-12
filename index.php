35053ba9ed188466cd5e9f50fbde928c209e8b65
legabi <legabi.dev@gmail.com>
2022-02-12 14:15:52 +0100

    add nav

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgamiWeb - Acceuil</title>
+    <link rel="icon" type="image/x-icon" href="/img/logo.png" />
+	<link rel="shortcut icon" type="image/x-icon" href="/img/logo.png" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta name="description" content="AgamiWeb vous présente son site !">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+
+    <meta property="og:title" content="AgamiWeb - Acceuil"/>
+    <meta property="og:url" content="https://agamiweb.ml/">
+    <meta property="og:locale" content="fr_FR">
+    <meta property="og:image" content="/img/logo.png"/>
+    <meta property="og:site_name" content="AgamiWeb"/>
+    <meta property="og:description" content="AgamiWeb vous présente son site !"/>
+    <meta name="theme-color" content="#0046FF"/>
+</head>
+<body>
+    <?php
+        include "nav.php";
+    ?>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <img src="/img/logo.png" alt="" class="navbar-brand" width="50px">
+    <a class="navbar-brand" href="#">AgamiWeb</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Acceuil
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nous contacter</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
